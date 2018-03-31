acd37ab3b3bc11d511b31fe62a9dfd9ef7382a6b
Aaron Rebmann <aaronrebmann@gmail.com>
2018-03-31 16:24:57 +0200

    Add script normalise.py
    Script to normalise data.

--- cleanseps.sh
+++ cleanseps.sh
@@ -1,6 +1,6 @@
 
 for file in *.txt; do
-    sed -E -e 's/\s+//' $file | sed -E  -e 's/\s+/,/g' | sed -E -e 's/,//3' > results_cleaned/$file
+    sed -E -e 's/\s+//' $file | sed -E  -e 's/\s+/,/g' | sed -E -e 's/,//3' > ../Radius_clean/$file
 done
 # 1. Entferne alle Folgen Leerzeichen in der Datei, die am Anfang einer Zeile stehen.
 # 2. Ersetze alle noch übrigen Folgen von Leerzeichen durch ein Komma.
